55f378a1876597166247ff3307791c2f4b6c63ca
Rebecca Dang <ph.rdang@gmail.com>
2020-10-18 18:21:16 -0700

    Make auto LinearOpModes, add roadrunner plugin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Blue.java
@@ -1,17 +1,35 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.Robot;
 
 @Autonomous(name="Blue", group="MCC")
-public class Blue extends OpMode {
-    @Override
-    public void init() {
-        // TODO write stuff
-    }
+public class Blue extends LinearOpMode {
+    Robot robot;
 
     @Override
-    public void loop() {
-        // TODO write stuff
+    public void runOpMode() {
+        // ----- INIT -----
+        // initialize robot hardware
+        robot = new Robot(hardwareMap);
+        robot.init();
+
+        // detect height of rings and pick a path
+        while (opModeIsActive()) {
+            // TODO write this
+        }
+
+        waitForStart();
+
+
+        // ----- START ----
+        // TODO write this - break up into helper functions
+        // run selected path
+
+
+        // ----- STOP -----
+        if (isStopRequested()) return;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Red.java
@@ -1,17 +1,37 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+
+// javadocs: https://first-tech-challenge.github.io/FtcRobotController/6.0.1/RobotCore/com/qualcomm/robotcore/eventloop/opmode/OpMode.html
 
 @Autonomous(name="Red", group="MCC")
-public class Red extends OpMode {
-    @Override
-    public void init() {
-        // TODO write stuff
-    }
+public class Red extends LinearOpMode {
+    Robot robot;
 
     @Override
-    public void loop() {
-        // TODO write stuff
+    public void runOpMode() {
+        // ----- INIT -----
+        // initialize robot hardware
+        robot = new Robot(hardwareMap);
+        robot.init();
+
+        // detect height of rings and pick a path
+        while (opModeIsActive()) {
+            // TODO write this
+        }
+
+        waitForStart();
+
+
+        // ----- START ----
+        // TODO write this - break up into helper functions
+        // run selected path
+
+
+        // ----- STOP -----
+        if (isStopRequested()) return;
     }
 }
