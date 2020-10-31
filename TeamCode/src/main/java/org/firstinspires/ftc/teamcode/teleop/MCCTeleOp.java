d0d75ea4fe524d2781d97a9577544d970d4012c2
planteatre <amogh1216@gmail.com>
2020-10-31 16:50:16 -0700

    added teleop methods: intake, shoot, moveArm, moveGrabber

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MCCTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MCCTeleOp.java
@@ -3,7 +3,10 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.hardware.Arm;
+import org.firstinspires.ftc.teamcode.hardware.Intake;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.hardware.Shooter;
 
 @TeleOp(name="MCC TeleOp")
 public class MCCTeleOp extends OpMode {
@@ -37,18 +40,45 @@ public class MCCTeleOp extends OpMode {
     }
 
     public void intake() {
-        // TODO this
+
+        while (gamepad2.left_bumper){
+            robot.intake.run(Intake.Mode.IN);
+        }
+
+        while (gamepad2.right_bumper){
+            robot.intake.run(Intake.Mode.OUT);
+        }
+
+        robot.intake.run(Intake.Mode.STOP);
+
     }
 
     public void shoot() {
-        // TODO this
+
+        if(gamepad2.a) {
+            robot.shooter.run(Shooter.Mode.SHOOT);
+        } else {
+            robot.shooter.run(Shooter.Mode.STOP);
+        }
     }
 
     public void moveArm() {
-        // TODO this
+
+        if (gamepad2.dpad_up){
+            robot.arm.setJointPosition(Arm.Position.STOWED);
+        } else if (gamepad2.dpad_down){
+            robot.arm.setJointPosition(Arm.Position.DOWN);
+        } else if (gamepad2.dpad_left){
+            robot.arm.setJointPosition(Arm.Position.CARRY);
+        }
     }
 
     public void moveGrabber() {
-        // TODO this
+
+        if (gamepad2.x) {
+            robot.arm.setGrabberMode(Arm.Mode.CLOSE);
+        } else if (gamepad2.y) {
+            robot.arm.setGrabberMode(Arm.Mode.OPEN);
+        }
     }
 }
