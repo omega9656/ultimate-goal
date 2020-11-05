bc9405f8de17b3b0ba49424ca18db75e5e89753d
Rebecca Dang <ph.rdang@gmail.com>
2020-11-04 22:00:32 -0800

    Fix shooter so it has flywheel, indexer
    Add methods:
    - speedUpFlywheel(): speeds up flywheel to default target velocity
    - speedUpFlywheel(double percent): speeds up flywheel to given target velocity
    - stopFlywheel()
    - isAtTargetVelocity(): checks if flywheel velocity is within default tolerance of default speed
    - isAtTargetVelocity(double targetVelocityPercent): checks if flywheel velocity is within default tolerance of given speed
    - isAtTargetVelocity(double targetVelocityPercent, double tolerance): checks if flywheel velocity is within given tolerance of given speed
    - pushRing(): sets position of indexer to SHOOT mode position
    - readyIndexer(): sets position of indexer to READY mode position
    - isRingPushed(): checks if current indexer position is SHOOT mode position
    - isIndexerReady(): checks if current indexer position is READY mode position
    
    Add constants:
    DEFAULT_SHOOTING_SPEED, DEFAULT_SHOOTING_SPEED_TOLERANCE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Shooter.java
@@ -2,26 +2,39 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Shooter {
-    public DcMotorEx motor; // gobilda 1:1 yellow jacket planetary
+    public DcMotorEx flywheel; // gobilda 1:1 yellow jacket planetary
+    public Servo indexer; // pushes ring into place
 
     // https://www.gobilda.com/5202-series-yellow-jacket-motor-1-1-ratio-24mm-length-6mm-d-shaft-6000-rpm-3-3-5v-encoder/
     public static final int TICKS_PER_REVOLUTION = 28;  // encoder countable events per revolution (output shaft)
     public static final int MAX_RPM = 5400;  // no-load speed @ 12VDC (tested)
     public static final int MAX_TICKS_PER_SEC = MAX_RPM * TICKS_PER_REVOLUTION / 60;
 
-    public Mode state;
+    public FlywheelMode flywheelMode;
+    public IndexerMode indexerMode;
 
-    public enum Mode {
+    public static final double DEFAULT_SHOOTING_SPEED = 0.8;  // 80% of max speed
+    public static final double DEFAULT_SHOOTING_SPEED_TOLERANCE = 0.01;  // +/- 1% of target velocity
+
+    public enum FlywheelMode {
         // TODO tune shooting speed - do variable velocities after MCC
-        SHOOT(getShootingSpeed(0.8)),  // optimal performance at 80% of max speed
-        STOP(0);
+        SHOOT,  // at target shooting velocity
+        SPEEDING_UP,  // speeding up the flywheel to target velocity
+        STOP,
+    }
 
-        public double velocity;
+    public enum IndexerMode {
+        // TODO tune servo positions
+        SHOOT(0),  // pushing the ring to the flywheel
+        READY(0);  // not pushing the ring
 
-        Mode(double velocity) {
-            this.velocity = velocity;
+        public double servoPos;
+
+        IndexerMode(double servoPos) {
+            this.servoPos = servoPos;
         }
     }
 
@@ -31,16 +44,18 @@ public class Shooter {
      * @param deviceManager  the robot's device manager
      */
     public Shooter(DeviceManager deviceManager) {
-        motor = deviceManager.shooter;
+        flywheel = deviceManager.flywheel;
+        indexer = deviceManager.indexer;
 
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // when power is set to 0, motors will stop and actively
         // resists any external force that might try to get the motor to move
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // set default state
-        state = Mode.STOP;
+        // set default states
+        flywheelMode = FlywheelMode.STOP;
+        indexerMode = IndexerMode.READY;
     }
 
     /**
@@ -54,21 +69,92 @@ public class Shooter {
     }
 
     /**
-     * Runs the shooter given a shooter <code>Mode</code>
-     * @param mode  the shooter's mode
+     * Speeds up the flywheel to the default shooting speed
+     * of 80% of max speed
      */
-    public void run(Mode mode) {
-        motor.setVelocity(mode.velocity);
-        state = mode;
+    public void speedUpFlywheel() {
+        flywheel.setVelocity(getShootingSpeed(DEFAULT_SHOOTING_SPEED));
+        flywheelMode = FlywheelMode.SPEEDING_UP;
     }
 
-    /** Runs the shooter motor */
-    public void shoot() {
-        run(Mode.SHOOT);
+    /**
+     * Speeds up the flywheel to a percentage of the
+     * max speed
+     * @param percent  percentage of the max speed as a decimal
+     */
+    public void speedUpFlywheel(double percent) {
+        flywheel.setVelocity(getShootingSpeed(percent));
+        flywheelMode = FlywheelMode.SPEEDING_UP;
     }
 
-    /** Stops the shooter motor */
-    public void stop() {
-        run(Mode.STOP);
+    /** Stops running the flywheel motor */
+    public void stopFlywheel() {
+        flywheel.setVelocity(0);
+    }
+
+    /**
+     * Returns <code>true</code> if the flywheel motor
+     * is at default target velocity (within the default shooting
+     * speed tolerance).
+     * @return <code>true</code> if the flywheel motor is at target velocity
+     */
+    public boolean isAtTargetVelocity() {
+        double targetVelocity = getShootingSpeed(DEFAULT_SHOOTING_SPEED);
+        return Math.abs(flywheel.getVelocity() - targetVelocity) <= (DEFAULT_SHOOTING_SPEED_TOLERANCE * targetVelocity);
+    }
+
+    /**
+     * Returns <code>true</code> if the flywheel motor
+     * is at a given target velocity (within the default shooting speed tolerance).
+     * @param targetVelocityPercent  target velocity of the flywheel motor, as a percent (decimal) of the max motor speed
+     * @return <code>true</code> if the flywheel motor is at target velocity
+     */
+    public boolean isAtTargetVelocity(double targetVelocityPercent) {
+        double targetVelocity = getShootingSpeed(targetVelocityPercent);
+        return Math.abs(flywheel.getVelocity() - targetVelocity) <= (DEFAULT_SHOOTING_SPEED_TOLERANCE * targetVelocity);
+    }
+
+    /**
+     * Returns <code>true</code> if the flywheel motor
+     * is at a given target velocity (within a given shooting
+     * speed tolerance).
+     * @param targetVelocityPercent  target velocity of the flywheel motor, as a percent (decimal) of the max motor speed
+     * @param tolerance  target velocity tolerance, as a percent (decimal) of the calculated target velocity
+     * @return <code>true</code> if the flywheel motor is at target velocity
+     */
+    public boolean isAtTargetVelocity(double targetVelocityPercent, double tolerance) {
+        double targetVelocity = getShootingSpeed(targetVelocityPercent);
+        return Math.abs(flywheel.getVelocity() - targetVelocity) <= (tolerance * targetVelocity);
+    }
+
+    /** Puts the indexer in SHOOT mode, pushing a ring toward the flywheel */
+    public void pushRing() {
+        indexer.setPosition(IndexerMode.SHOOT.servoPos);
+        indexerMode = IndexerMode.SHOOT;
+    }
+
+    /** Puts the indexer in READY mode */
+    public void readyIndexer() {
+        indexer.setPosition(IndexerMode.READY.servoPos);
+        indexerMode = IndexerMode.READY;
+    }
+
+    /**
+     * Returns <code>true</code> if the indexer has pushed the ring
+     * (the indexer is currently in SHOOT position)
+     * @return <code>true</code> if the indexer has pushed the ring
+     */
+    public boolean isRingPushed() {
+        return indexer.getPosition() == IndexerMode.SHOOT.servoPos;
+    }
+
+    /**
+     * Returns <code>true</code> if the indexer is ready to
+     * push another ring toward the flywheel (the indexer is
+     * currently in READY position)
+     * @return <code>true</code> if the indexer is ready to push another ring
+     */
+    public boolean isIndexerReady() {
+        return indexer.getPosition() == IndexerMode.READY.servoPos;
     }
 }
